13151aa8594091c341d35ca333296c9905fe6035
sabitaghorai <108499262+sabitaghorai@users.noreply.github.com>
2023-08-07 10:42:37 +0530

    Add files via upload

---
+++ break_continue.py
@@ -0,0 +1,6 @@
+i=0
+while i<100:
+    if i>50:
+        break
+    print(i)
+    i+=1
---
+++ looping.py
@@ -0,0 +1,20 @@
+#while loop
+# names=input("Enter the names to wish separated by comma:").split(",")
+# print(names)
+# i=0
+# while i<len(names):
+#     print(f"Happy birthday {names[i]}")
+#     i+=1
+# while True: #is used where we do not the limit
+#    print(i)    
+#    i+=1
+# for loop
+# names=input("Enter the names to wish separated by comma:").split(",")  
+
+# for i in range(len(names)): 
+#    print(f"Happy birthday {names[i]}")
+
+#iterator=iter() and next()
+
+iterator =iter([2,3,4,4,5])   
+print(next(iterator))
